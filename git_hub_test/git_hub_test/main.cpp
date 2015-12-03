e46dd1e6162a847b8b9c74cb4276cbd6308660a2
JIN <jin@ex-corp.co.jp>
2015-12-03 10:22:16 +0900

    first time push!

---
+++ git_hub_test/git_hub_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_hub_test
+//
+//  Created by jin on 2015. 12. 3..
+//  Copyright © 2015년 test. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
