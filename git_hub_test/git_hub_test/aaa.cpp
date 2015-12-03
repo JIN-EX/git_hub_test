c4899253e05ab18ae6cd48016971e29082628a54
JIN <jin@ex-corp.co.jp>
2015-12-03 10:27:59 +0900

    class add

---
+++ git_hub_test/git_hub_test/aaa.cpp
@@ -0,0 +1,24 @@
+//
+//  aaa.cpp
+//  git_hub_test
+//
+//  Created by jin on 2015. 12. 3..
+//  Copyright © 2015년 test. All rights reserved.
+//
+
+#include "aaa.hpp"
+
+aaa::aaa()
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+}
+
+aaa::~aaa()
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+}
+
+void aaa::func_A_1()
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+}
---
+++ git_hub_test/git_hub_test/aaa.hpp
@@ -0,0 +1,32 @@
+//
+//  aaa.hpp
+//  git_hub_test
+//
+//  Created by jin on 2015. 12. 3..
+//  Copyright © 2015년 test. All rights reserved.
+//
+
+#ifndef aaa_hpp
+#define aaa_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+class aaa {
+    
+public:
+    aaa();
+    virtual ~aaa();
+    
+private:
+    
+    void func_A_1();
+    
+protected:
+    
+    
+    
+};
+
+
+#endif /* aaa_hpp */
